2ef3aa751ad169912a85e8d1991feef5a9be4fc6
Richo Arbianto <richoarbianto@MBA-M1.local>
2024-07-15 17:46:15 +0700

    Menghapus yang tidak perlu

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-payload = {'isOverlayRequired': False,
-    'apikey': 'K83554843688957',
-    'language': 'eng',
-    }
-with open('image.jpg', 'rb') as f:
-    r = requests.post('https://api.ocr.space/parse/image', files={'image.jpg': f}, data=payload)
-print(r.content.decode())
